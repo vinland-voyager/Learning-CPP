298e62fc3c575968fdd64435e3874fccdef42b62
Aayush Kumar Jaiswal <akjindividual@gmail.com>
2025-02-13 10:51:56 +0530

    practice

---
+++ Print all the numbers between 1 and 100 which are divisible by 5.cpp
@@ -0,0 +1,16 @@
+//
+// Created by Aayush Kumar Jaiswal on 13/02/25.
+// in class
+
+#include <iostream>
+using namespace std;
+int main(){
+  int num=1;
+  while(num<=100)
+  {
+    if(num%5==0)
+      cout<<num<<"\t";
+    num++;
+  }
+  return 0;
+}
--- main.cpp
+++ main.cpp
@@ -1,15 +1,15 @@
 //
 // Created by Aayush Kumar Jaiswal on 13/02/25.
-//
+// in class
 
 #include <iostream>
 using namespace std;
-
-int main()
-{
+int main(){
     int num=1;
-    while (num<=50){
-        cout<<num<<"\t";
+    while(num<=100)
+    {
+        if(num%5==0)
+            cout<<num<<"\t";
         num++;
     }
     return 0;
